87f543261414147e7d8518a016fdd54ca44ead98
Dave Schwantes <dave.schwantes@gmail.com>
2021-01-27 23:00:37 +0000

    added json response

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'gemoji'
+require 'json'
 
 Dir["./models/*.rb"].each {|file| require file }
 
@@ -10,13 +11,27 @@ class App < Sinatra::Base
   end
 
   get '/emjoify/:term' do
+    content_type :json
+
     term = params[:term]
     emoji = Emoji.find_by_alias(term)&.raw
+    status = ""
+    message = ""
+    content = ""
+
     if emoji
-      emoji
+      status = "success"
+      content = emoji
     else
-      "sorry couldn't find an emoji for #{term}"
+      status = "failure"
+      message = "sorry couldn't find an emoji for #{term}"
     end
+
+    {
+      status: status,
+      message: message,
+      content: content
+    }.to_json
   end
 
   run! if app_file == $0
